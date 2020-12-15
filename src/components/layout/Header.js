c0c26bca557edf18a1a6d7d9512e2f95f37610de
Tomas Diez <ltomasdiez@gmail.com>
2020-12-15 00:21:41 -0300

    React: Example

---
+++ src/components/layout/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+function Header(){
+    return(
+        <header style={headerStyle}>
+            <h1>
+                TodoList
+            </h1>
+            <Link style={headerStyle} to="/">Home</Link>|<Link style={headerStyle} to="/about">About</Link>
+        </header>
+    )
+};
+
+const headerStyle={
+    background: '#333',
+    color:'#fff',
+    textAlign:'center',
+    padding: '10px'
+}
+
+export default Header;
